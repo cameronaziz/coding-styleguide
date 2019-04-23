2ae5e24431163e69f167bf8e56a6a280d4bc1294
Cameron Aziz <cameronaziz@me.com>
2019-04-22 21:35:54 -0400

    that annoying border

--- src/pages/search/ui.ts
+++ src/pages/search/ui.ts
@@ -9,6 +9,9 @@ export const StyledH1 = styled.h1`
 export const StyledInput = styled.input`
   font-size: 16px;
   height: 40px;
+  &:focus {
+    outline: 0;
+  }
 `
 
 // Since we are using typescript, our compiler will error and we do not need to use lodash.get()
@@ -21,4 +24,7 @@ export const StyledButton = styled.button`
   padding: 15px 32px;
   text-align: center;
   text-decoration: none;
+  &:focus {
+    outline: 0;
+  }
 `
